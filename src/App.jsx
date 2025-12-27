ac5ab7c116541541861307e56df83575d05ea4bf
GOKUL KRISHNAN B <gokulkrishnanb3@gmail.com>
2025-12-27 10:10:31 +0530

    Initial commit - task manager

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Greeting from './core-concepts/Greeting'
 import GreetingClass from './core-concepts/GreetingClass'
 import Helloworld from './core-concepts/helloworld'
+import JSXExample from './core-concepts/jsxexample'
 
 function App() {
   // return <div> <Helloworld/>
   // <Greeting/>
   // </div>
-  return <GreetingClass/>
+  return <JSXExample/>
 }
 
 export default App
---
+++ src/core-concepts/jsxexample.jsx
@@ -0,0 +1,10 @@
+import "../App.css";
+
+function JSXExample() {
+    const name = "Gokul Krishnan B";
+    return<> <h1 className="read-the-docs">Hello {name}  </h1>
+    <p style={{color:"red"}}>Helloworld </p>
+    </> ;
+
+}
+export default JSXExample;
